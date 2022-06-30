b2375ee1d7cf8b97a2a30575002c2f4c095d8810
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2022-06-30 08:28:56 +0200

    Add test case for default config values

--- test/components/gdik-map/GDIKMap.spec.js
+++ test/components/gdik-map/GDIKMap.spec.js
@@ -1,4 +1,5 @@
 import GDIKMap from "../../../src/components/gdik-map/GDIKMap";
+import * as defaultConfig from "../../../src/components/gdik-map/assets/config.json";
 
 describe("Init gdik-map", () => {
     it("can create gdik-map component", () => {
@@ -7,5 +8,15 @@ describe("Init gdik-map", () => {
         const component = new GDIKMap();
 
         expect(component).toBeDefined();
+    });
+
+    it("use values from default config", async () => {
+        const component = new GDIKMap();
+
+        await component.connectedCallback();
+
+        expect(Number(component.getAttribute("lon"))).toBe(defaultConfig.portal.startCenter[0]);
+        expect(Number(component.getAttribute("lat"))).toBe(defaultConfig.portal.startCenter[1]);
+        expect(component.getAttribute("layer")).toBe("1001");
     });
 });
