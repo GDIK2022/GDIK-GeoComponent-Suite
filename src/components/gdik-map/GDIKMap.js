c6be3ab30e2192b99e1a7d7a88046f85bbdeece4
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2022-07-07 10:44:46 +0200

    Add ol controls Zoom and FullScreen

--- src/components/gdik-map/GDIKMap.js
+++ src/components/gdik-map/GDIKMap.js
@@ -2,6 +2,8 @@ const merge = require("deepmerge"),
     ID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 import DoubleClickZoom from "ol/interaction/DoubleClickZoom";
+import Zoom from 'ol/control/Zoom';
+import FullScreen from 'ol/control/FullScreen';
 
 import mapsAPI from "masterportalAPI/src/maps/api.js";
 
@@ -117,6 +119,9 @@ export default class GDIKMap extends HTMLElement {
         this.container.innerHTML = "";
 
         map = mapsAPI.map.createMap({...config.portal, layerConf: config.services}, "2D");
+
+        map.addControl(new Zoom());
+        map.addControl(new FullScreen())
 
         dobleClickZoom = this.getInteractionByClass(map, DoubleClickZoom);
         map.removeInteraction(dobleClickZoom);
