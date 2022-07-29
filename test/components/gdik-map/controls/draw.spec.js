263524f66df102a543c960f75265e46ab63cb25b
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2022-07-29 13:19:54 +0200

    Test complete state changes

--- test/components/gdik-map/controls/draw.spec.js
+++ test/components/gdik-map/controls/draw.spec.js
@@ -43,25 +43,35 @@ describe("Draw Control", () => {
         expect(control.featureSource).toBeDefined();
     });
 
-    it("should disable draw and enable modify and remove when feature added", () => {
+    it("should toggle controls depending on feature source", () => {
         const control = new DrawControl({drawType: "Point"}),
             map = new Map(),
+            feature = new Feature({geometry: new Point([1, 1])}),
             addInteractionSpy = jest.spyOn(map, "addInteraction");
 
         control.setMap(map);
 
         expect(addInteractionSpy).toHaveBeenCalledTimes(2);
 
+        // Initial state, no feature in source
         expect(control.element.firstChild.nodeName).toBe("BUTTON");
         expect(control.element.firstChild.disabled).toBe(true);
         expect(control.drawInteraction.getActive()).toBe(true);
         expect(control.modifyInteraction.getActive()).toBe(false);
 
-        control.featureSource.addFeature(new Feature({geometry: new Point([1, 1])}));
+        control.featureSource.addFeature(feature);
 
+        // State after feature added to source
         expect(control.element.firstChild.disabled).toBe(false);
         expect(control.drawInteraction.getActive()).toBe(false);
         expect(control.modifyInteraction.getActive()).toBe(true);
+
+        control.featureSource.removeFeature(feature);
+
+        // State after feature removed from source
+        expect(control.element.firstChild.disabled).toBe(true);
+        expect(control.drawInteraction.getActive()).toBe(true);
+        expect(control.modifyInteraction.getActive()).toBe(false);
 
     });
 });
