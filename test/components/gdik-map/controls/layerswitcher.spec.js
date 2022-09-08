9d28a6aa37d3b093803ec137f078c2cccca94dac
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2022-09-08 17:35:35 +0200

    Add toggle layerswitcher visibility test

--- test/components/gdik-map/controls/layerswitcher.spec.js
+++ test/components/gdik-map/controls/layerswitcher.spec.js
@@ -74,7 +74,7 @@ describe("Layerswitcher", () => {
         });
     });
 
-    it("should change visible background layer", (done) => {
+    it("should change visible background layer", async () => {
         const control = new LayerswitcherControl(layerManager);
 
         control.setMap(map);
@@ -88,15 +88,46 @@ describe("Layerswitcher", () => {
         expect(layerEntries[1].className).toBe("list-group-item");
         expect(map.getLayers().item(1).getVisible()).toBe(false);
 
-        control.handleBackgroundLayerChange("1002").then(() => {
+        await control.handleBackgroundLayerChange("1002").then(() => {
             layerEntries = control.element.querySelectorAll("div ul li");
 
             expect(layerEntries[0].className).toBe("list-group-item");
             expect(map.getLayers().item(0).getVisible()).toBe(false);
             expect(layerEntries[1].className).toBe("list-group-item active");
             expect(map.getLayers().item(1).getVisible()).toBe(true);
-            done();
         });
 
     });
+
+    it("should toggle open and close element", () => {
+        const control = new LayerswitcherControl(layerManager);
+
+        let opener = control.element.querySelector("button"),
+            listContainer = control.element.querySelector("div div"),
+            closer = control.element.querySelector("div div button");
+
+        expect(opener.className).toBe("");
+        expect(listContainer.className).toBe("list-container hidden");
+        expect(closer.className).toBe("closer");
+
+        opener.click();
+
+        opener = control.element.querySelector("button");
+        listContainer = control.element.querySelector("div div");
+        closer = control.element.querySelector("div div button");
+
+        expect(opener.className).toBe("hidden");
+        expect(listContainer.className).toBe("list-container");
+        expect(closer.className).toBe("closer");
+
+        closer.click();
+
+        opener = control.element.querySelector("button");
+        listContainer = control.element.querySelector("div div");
+        closer = control.element.querySelector("div div button");
+
+        expect(opener.className).toBe("");
+        expect(listContainer.className).toBe("list-container hidden");
+        expect(closer.className).toBe("closer");
+    })
 });
