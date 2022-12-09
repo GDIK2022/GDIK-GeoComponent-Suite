332689c6526c5fe57f4faa35b2d0d1b63b4f0869
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2022-12-09 15:20:08 +0100

    Implement geolocation functionality

--- src/components/gdik-geolocation/geolocationControl.js
+++ src/components/gdik-geolocation/geolocationControl.js
@@ -1,4 +1,5 @@
 import {Control} from "ol/control";
+import Geolocation from "ol/Geolocation";
 
 export default class GeolocationControl extends Control {
 
@@ -12,9 +13,29 @@ export default class GeolocationControl extends Control {
         containerDiv.className = "ol-control gdik-geolocation";
         super({element: containerDiv});
 
+        button.onclick = this.geolocate.bind(this);
+
     }
 
     setMap (map) {
         super.setMap(map);
+        this.view = map.getView();
+
+        this.geolocation = new Geolocation({
+            trackingOptions: {
+                enableHighAccuracy: true
+            },
+            projection: this.view.getProjection()
+        });
+    }
+
+    geolocate () {
+        this.geolocation.setTracking(true);
+
+        this.geolocation.once("change:position", () => {
+            this.view.setCenter(this.geolocation.getPosition());
+            this.geolocation.setTracking(false);
+        });
+
     }
 }
