683e89892dc57b667f49000bcf44f5f88449beeb
Kai Culemann <kai.culemann@lgln.niedersachsen.de>
2024-04-04 14:52:11 +0200

    Zoom to selected search result

--- test/components/gcs-search/searchControl.spec.js
+++ test/components/gcs-search/searchControl.spec.js
@@ -4,9 +4,6 @@ import SearchControl from "../../../src/components/gcs-search/searchControl";
 import * as defaultConfig from "../../../src/components/gcs-map/assets/config.json";
 import * as searchResult from "./assets/searchResults.json";
 
-import {enableFetchMocks} from "jest-fetch-mock";
-enableFetchMocks();
-
 describe("Search", () => {
     const options = {
         searchUrl: "https://search",
@@ -74,6 +71,42 @@ describe("Search", () => {
 
 
         expect(map.getView().getCenter()).toEqual(resultCoord);
+        // default fallback zoom level
+        expect(map.getView().getZoom()).toBe(11);
+    });
+
+    it("should move to search result bbox instead of coordinate and fit zoom to bbox", () => {
+        const control = new SearchControl(options),
+            resultText = "teststring",
+            resultCoord = [450010.0, 5500010.0],
+            resultBBox = [441632.41251335153, 5881752.268513008, 452479.6627111422, 5895430.011182868];
+
+        map.addControl(control);
+
+        control.showResult(resultText, resultCoord, resultBBox);
+
+        expect(control.element.firstChild.value).toBe(resultText);
+
+        expect(map.getView().getCenter()).toEqual([447056.0376122469, 5888591.139847938]);
+        // default fallback zoom level
+        expect(map.getView().getZoom()).toBe(3);
+    });
+
+    it("should zoom map to defined zoom level", () => {
+        const fallbackResultZoomLevel = 10,
+            control = new SearchControl({fallbackResultZoomLevel: fallbackResultZoomLevel, ...options}),
+            resultText = "teststring",
+            resultCoord = [450010.0, 5500010.0];
+
+        map.addControl(control);
+
+        control.showResult(resultText, resultCoord);
+
+        expect(control.element.firstChild.value).toBe(resultText);
+
+
+        expect(map.getView().getCenter()).toEqual(resultCoord);
+        expect(map.getView().getZoom()).toBe(fallbackResultZoomLevel);
     });
 
 });
